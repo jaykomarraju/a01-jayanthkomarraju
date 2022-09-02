b91e8210da8761de6ca292465fc4b263bf4c9743
jaykomarraju <jay.komarraju@gmail.com>
2022-09-01 23:23:29 -0400

    First release of A1

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 // Require http module
+var http = require('http');
 
 // Require fs module
+var fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
+var argv = require('minimist')(process.argv.slice(2));
+
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
@@ -10,6 +14,34 @@
 // Make this const default to port 3000 if there is no argument given for `--port`.
 
 // Use the fs module to create an arrow function using `fs.readFile`.
+// fs.readFile('./public/index.html', (err, data) => {
+//     if (err) throw err;
+//     const server = http.createServer((req,res) => {
+//         res.statusCode(200);
+//         res.writeHead('text/html');
+//         res.end(data);
+//         console.log(`Server listening on port ${port}`)
+//     }).listen(3000);
+// })
+fs.readFile('./public/index.html', (err,data) => {
+    if (err) {
+        throw err;
+    }
+    else{
+        console.log("hi")
+        const server = http.createServer((req,res) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data)
+        }).listen(7001, () => {
+            console.log('Server started at 7001');
+           });
+        // const server = http.createServer((req,res) => {
+        //     res.statusCode(200)
+        //     res.setHeader('text/html')
+        //     res.end(data)
+        // }).listen(3000)
+    }
+})
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
